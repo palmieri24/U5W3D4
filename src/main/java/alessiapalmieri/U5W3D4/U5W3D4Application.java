50a9a237a56e06854e58ba0e791f7437e0023a2a
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2024-02-22 17:02:44 +0100

    Initial commit

---
+++ src/main/java/alessiapalmieri/U5W3D4/U5W3D4Application.java
@@ -0,0 +1,13 @@
+package alessiapalmieri.U5W3D4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D4Application.class, args);
+	}
+
+}
---
+++ src/test/java/alessiapalmieri/U5W3D4/U5W3D4ApplicationTests.java
@@ -0,0 +1,13 @@
+package alessiapalmieri.U5W3D4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
